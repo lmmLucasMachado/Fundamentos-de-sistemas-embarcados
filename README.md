3cf9af4aceff25faf96b7fe57925ae52fb9e7943
lmmLucasMachado <lmlucasmachadom@gmail.com>
2020-09-22 11:47:01 -0300

    Fix bugs of sintax

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Fundamentos-de-sistemas-embarcados
+
 Este repositório tem como objetivo armazenar as atividades da disciplina de fundamentos de sistemas embarcados
--- trabalho_1/grandezas_bme280.py
+++ trabalho_1/grandezas_bme280.py
@@ -3,8 +3,8 @@ import bme280
 import RPi_I2C_driver
 import time
 
-portai2c = 1
-endereco = 0x7
+porta_i2c = 1
+endereco = 0x76
 bus = smbus2.SMBus(porta_i2c)
 
 calibracao_parametros = bme280.load_calibration_params(bus, endereco)
@@ -13,11 +13,11 @@ newlcd = RPi_I2C_driver.lcd()
 
 
 while True:
-	dado = bme280.samples(bus, endereco, calibracao_parametros)
+	dado = bme280.sample(bus, endereco, calibracao_parametros)
 	
-	newlcd.clear()
+	newlcd.lcd_clear()
 	newlcd.lcd_display_string("T: " + str(round(dado.temperature, 2)) + " U: " + str(round(dado.humidity, 2)), 1)
-	newlcd.lcd_display_string(" P: " + str(round(dado.pressure, 2)), 2)
+	newlcd.lcd_display_string("P: " + str(round(dado.pressure, 2)) + "  LM", 2)
 	
 	time.sleep(1)
 	
